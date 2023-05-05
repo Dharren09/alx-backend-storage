f9e1022960b9af493209ad094e4d7519f5861c3c
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-05 14:59:50 +0300

    Advanced SQL

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,24 +2,13 @@
 -- that computes and store the average score of a student
 -- an average score can be a decimal
 -- procedure is taking 1 input and is linking to an existing users
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE num_corrections INT;
-    DECLARE avg_score FLOAT;
-    
-
-    SELECT SUM(score), COUNT(*)
-    INTO total_score, num_corrections
-    FROM corrections
-    WHERE user_id = user_id;
-
-    IF num_corrections > 0 THEN
-	SET avg_score = total_score / num_corrections;
-	UPDATE users
-	SET average_score = avg_score
-	WHERE id = user_id;
-    END IF;
+    DECLARE average_score DECIMAL (10, 2);
+    SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
+    UPDATE users SET average_score = CASE WHEN average_score > 0 THEN average_score
+    ELSE 0 END WHERE id = user_id;
 END $$
 DELIMITER ;
