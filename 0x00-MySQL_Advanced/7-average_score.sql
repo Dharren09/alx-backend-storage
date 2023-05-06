caccfe6c6a93566972581576eff85a569940cfcf
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-06 11:12:39 +0300

    Advanced SQL

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,11 +4,19 @@
 -- procedure is taking 1 input and is linking to an existing users
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average_score DECIMAL (10, 2);
-    SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
-    UPDATE users SET average_score = CASE WHEN average_score > 0 THEN average_score
-    ELSE 0 END WHERE id = user_id;
+    DROP TABLE IF EXISTS average_scores;
+    CREATE TABLE average_scores (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	average_score DECIMAL(10,2) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    INSERT INTO average_scores (user_id, average_score)
+    SELECT user_id, AVG(score)
+    FROM corrections
+    WHERE user_id = user_id
+    GROUP BY user_id;
 END $$
 DELIMITER ;
