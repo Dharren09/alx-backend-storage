4054aaea549f9c8232c88d9e8d96127c4a178e79
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-06 11:36:38 +0300

    Advanced SQL

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,17 +6,12 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DROP TABLE IF EXISTS average_scores;
-    CREATE TABLE average_scores (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	user_id INT NOT NULL,
-	average_score DECIMAL(10,2) NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-    INSERT INTO average_scores (user_id, average_score)
-    SELECT user_id, AVG(score)
-    FROM corrections
-    WHERE user_id = user_id
-    GROUP BY user_id;
+    UPDATE users
+    SET average_score = (
+	SELECT AVG(score)
+        FROM corrections
+        WHERE user_id = user_id
+    )
+    WHERE id = user_id;
 END $$
 DELIMITER ;
