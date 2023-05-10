28005bdcdd701a37fba2007f6eece3371e86d43b
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-10 19:17:51 +0300

    Redis Basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 """create a cache class and two methods that stores an instance"""
 
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
@@ -21,3 +21,17 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable, default=None):
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str):
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str):
+        return self.get(key, lambda x: int(x))
