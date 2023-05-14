2fc469ed87a063bf0103a124b3efe9cccd19925f
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-14 02:26:06 +0000

    Redis Basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,6 +17,18 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args):
+        input_list_key = f"{method.__qualname__}:inputs"
+        output_list_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_list_key, str(args))
+        output = method(self, *args)
+        self._redis.rpush(output_list_key, str(output))
+
+        return output
+    return wrapper
 
 class Cache:
     def __init__(self):
@@ -28,6 +40,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores the given data in Redis and returns the key"""
         key = str(uuid.uuid4())
