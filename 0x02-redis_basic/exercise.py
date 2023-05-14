e86b0450dd6060faec6f2e81937b4b6f975f9a6a
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-14 01:35:26 +0000

    Redis Basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,33 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn=None):
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is None:
+            return value
+        return fn(value)
+
+    def get_str(self, key):
+        """return str rep."""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key):
+        """ return integers"""
+        return self.get(key, int)
+
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
