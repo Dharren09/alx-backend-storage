e461faad7179b575a695df79fa891f60f67eb376
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-16 14:33:28 +0300

    Redis Bassics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-"""creates a class, init method and a store instance tthat take in a
-data argument and returns a str"""
+"""
+A class that represents a cache with method decorators and a replay function.
+"""
 
 import redis
 from typing import Union, Callable, Optional
 import uuid
 from functools import wraps
 
+
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """get qualified name of the method"""
         key = method.__qualname__
-        """get increment of the count"""
         self._redis.incr(key)
         return method(self, *args, **kwargs)
+
     return wrapper
+
 
 def call_history(method: Callable) -> Callable:
     @wraps(method)
@@ -29,49 +31,28 @@ def call_history(method: Callable) -> Callable:
         self._redis.rpush(output_list_key, str(output))
 
         return output
+
     return wrapper
 
-def replay(method: Callable) -> None:
-    """Display the history of calls of a particular function"""
-    if not callable(method):
-        raise TypeError("method must be callable")
 
-    r = redis.Redis()
-    f_name = method.__qualname__
-    n_calls = r.get(f_name)
-    if n_calls is None:
-        n_calls = 0
-    print(f'{f_name} was called {n_calls} times:')
-
-    ins = r.lrange(f_name + ":inputs", 0, -1)
-    outs = r.lrange(f_name + ":outputs", 0, -1)
-
-    for i, o in zip(ins, outs):
-        try:
-            i = i.decode('utf-8')
-        except Exception:
-            i = ""
-        try:
-            o = o.decode('utf-8')
-        except Exception:
-            o = ""
-
-        print(f'{f_name}(*{i}) -> {o}')
+def replay(instance: object, method_name: str):
+    redis_client = redis.Redis()
+    inputs = redis_client.lrange(f"{method_name}:inputs", 0, -1)
+    outputs = redis_client.lrange(f"{method_name}:outputs", 0, -1)
+    count = redis_client.get(method_name).decode("utf-8")
+    print(f"{method_name} was called {count} times:")
+    for i, j in zip(inputs, outputs):
+        print(f"{method_name}(*{i.decode('utf-8')}) -> {j.decode('utf-8')}")
 
 
 class Cache:
     def __init__(self):
-        """
-        initilaizes a redis database
-        creates a private variable then flushes out
-        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """stores the given data in Redis and returns the key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -85,11 +66,9 @@ class Cache:
         return fn(value)
 
     def get_str(self, key):
-        """return str rep."""
         return self.get(key, lambda d: d.decode("utf-8"))
 
     def get_int(self, key):
-        """ return integers from redis database"""
         return self.get(key, int)
 
 
@@ -105,3 +84,5 @@ if __name__ == "__main__":
     for value, fn in TEST_CASES.items():
         key = cache.store(value)
         assert cache.get(key, fn=fn) == value
+
+    replay(cache, "Cache.store")
